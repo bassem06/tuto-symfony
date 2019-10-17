9cb3155f215382df04568fbdaf4fededf9845737
bassem06 <raouafi.bassem06@gmail.com>
2019-10-17 10:39:07 +0100

    commit : Controlleur les Sessions

--- src/Bassem/FirstBundle/Controller/DefaultController.php
+++ src/Bassem/FirstBundle/Controller/DefaultController.php
@@ -3,11 +3,22 @@
 namespace Bassem\FirstBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
-    public function indexAction()
+    public function indexAction(Request $request)
     {
+        //récupérer notre session
+        $session=$request->getSession();
+        //essaye de récupérer ma variable de session
+       $maVariable=$session->get('maVariable');
+       if(isset($maVariable)){
+           echo 'Session déja ouverte : '.$maVariable;
+       }else {
+           echo 'Session vide => init session';
+           $session->set('maVariable','bassem');
+       }
         return $this->render('BassemFirstBundle:Default:index.html.twig');
     }
 
